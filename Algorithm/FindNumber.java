ceabb7ff9aa15f63f4ebce24a546eaf07d27b77c
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-09 00:36:26 +0530

    Added: Guess Number between range program in java

--- Algorithm/FindNumber.java
+++ Algorithm/FindNumber.java
@@ -1,2 +1,33 @@
-package org.example.Algorithm;public class FindNumber {
+/*
+Desc -> takes a command-line argument N, asks you to think of a number
+between 0 and N-1, where N = 2^n, and always guesses the answer with n
+questions.
+b. I/P -> the Number N and then recursively ask true/false if the number is between
+a high and low value
+c. Logic -> Use Binary Search to find the number
+d. O/P -> Print the intermediary number and the final answer
+ */
+package org.example.Algorithm;
+import java.util.Scanner;
+public class FindNumber {
+    int  guessNumber(int low,int high,Scanner scanner){
+        if(low==high) return low;
+        if(low>high) return -1;
+        int mid= low+(high-low)/2;
+        System.out.println("Press true if your number is between "+low+" and "+mid +" else false");
+        boolean response= scanner.nextBoolean();
+        if(response){
+          return  guessNumber(low,mid,scanner);
+        }
+        else return guessNumber(mid+1,high,scanner);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Please Type Number: ");
+        int n= scanner.nextInt();
+        int range= (int) Math.pow(2,n);
+        FindNumber fn= new FindNumber();
+        System.out.println(fn.guessNumber(0,range-1,scanner));
+    }
 }
