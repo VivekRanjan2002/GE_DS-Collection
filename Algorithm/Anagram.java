c9df6b56fdcae870471f6096068eda10f0535cce
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-09 00:35:02 +0530

    Added: Anagram checker Program in java

--- Algorithm/Anagram.java
+++ Algorithm/Anagram.java
@@ -1,2 +1,33 @@
-package org.example.Algorithm;public class Anagram {
+/*
+a. Desc -> One string is an anagram of another if the second is simply a
+rearrangement of the first. For example, 'heart' and 'earth' are anagrams...
+b. I/P -> Take 2 Strings as Input such abcd and dcba and Check for Anagrams
+c. O/P -> The Two Strings are Anagram or not....
+ */
+package org.example.Algorithm;
+import java.util.Arrays;
+import java.util.Hashtable;
+
+public class Anagram {
+
+    boolean checker(String str1, String str2) {
+        char[] temp1 = str1.toCharArray();
+        char[] temp2 = str2.toCharArray();
+        Arrays.sort(temp1);
+        Arrays.sort(temp2);
+        if(temp1.length!=temp2.length) return false;
+        for(int i=0;i<temp1.length;i++){
+            if(temp1[i]!=temp2[i]) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Anagram ag= new Anagram();
+        System.out.println(ag.checker("abcd","dcba")); // true
+        System.out.println(ag.checker("aaab","baaa")); // true
+        System.out.println(ag.checker("aabb","baba")); // true
+        System.out.println(ag.checker("aabba","bababa")); // false
+        System.out.println(ag.checker("hence","encfh"));  // false
+
+    }
 }
